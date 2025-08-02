f696b051cc671582ebc1fbd22f4fa2a038d16749
Zeeshu512141 <queenmirha09@gmail.com>
2025-08-02 15:09:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~yM9k0BQB#87-zQ49pDmQQKmQW0HkXTkcIvnCGwR1d5ktsE_9-pzA",  
 //chatbot integration 
 CHATBOT: process.env.CHATBOT || "on",
 // add your Session Id 
